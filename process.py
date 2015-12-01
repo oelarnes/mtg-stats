4c23db1e0a27be5309ad10ae35688982b7fc6bad
Joel Barnes <oelarnes@gmail.com>
2015-11-30 21:43:35 -0500

    process elim rounds, ready to process all raw results!

--- process.py
+++ process.py
@@ -1,13 +1,10 @@
 import requests
 
 from mtgdb import Cursor
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 RAW_TABLE_NAME = 'results_raw'
-RAW_COL_NAMES = ['table_id', 'p1_name_raw', 'p1_country', 'result_raw', 'vs', 'p2_name_raw', 'p2_country', 'round_num', 'event_id']
+RAW_COL_NAMES = ['table_id', 'p1_name_raw', 'p1_country', 'result_raw', 'vs', 'p2_name_raw', 'p2_country', 'round_num', 'event_id', 'elim']
 MAGIC_URL = 'http://magic.wizards.com'
 
 def clean_magic_link(url):
@@ -30,14 +27,116 @@ def all_event_links():
     else:
         r.raise_for_status()
 
-def all_rounds_info(event_link):
+def upload_round_results(results_table, event_id, round_num):
+    # results_table must all have same round_num and represent all results for that round!!
+    print '==========Processing Results for Event {}, Round {}=========='.format(event_id, round_num)
+    cursor = Cursor()
+    print 'Writing {} rows'.format(len(results_table))
+    cursor.insert(RAW_TABLE_NAME, results_table)
+    cursor.close()
+    cursor = Cursor()
+    print 'New {} row count: {}'.format(RAW_TABLE_NAME, cursor.execute('select count(1) from {}'.format(RAW_TABLE_NAME))[0][0])
+    cursor.close(commit=False)
+
+def standardize_name(name):
+    first_last = name.split(' ')
+    return ' '.join(first_last[1:]) + ', ' + first_last[0]
+
+def elim_results(soup, event_id, max_round_num):
+    ELIM_ERR_MSG = 'Could not interpret elimation round results for event {}'.format(event_id)
+    bracket_pairs = soup.find('div', class_='top-bracket-slider').find_all('div', class_='dual-players')
+    results_table = []
+    print '{} matches found in elimination rounds'.format(len(bracket_pairs))
+    for idx, pair in enumerate(bracket_pairs):
+        players = list(pair.find_all('div', class_='player'))
+        p1 = players[0].text.strip().lstrip('()12345678 ')
+        p2 = players[1].text.strip().lstrip('()12345678 ')
+        p1_part = p1.partition(',')
+        p2_part = p2.partition(',')
+        strong = pair.find('strong').text.strip().lstrip('()12345678 ')
+        result_raw = ''
+        if strong == p1 or len(p1_part[2]) > 0 and len(p2_part[2]) == 0:
+            result_raw = 'Won ' + p1_part[2]
+        if strong == p2 or len(p2_part[2]) > 0 and len(p2_part[2]) == 0:
+            if len(result_raw) > 0:
+                raise Exception(ELIM_ERR_MSG)
+            result_raw = 'Lost ' + p2_part[2]
+        if len(result_raw)==0:
+            raise Exception(ELIM_ERR_MSG)
+        p1_name_raw = standardize_name(p1_part[0])
+        p2_name_raw = standardize_name(p2_part[0])
+        if len(bracket_pairs) == 7:
+            if idx < 4:
+                round_num = max_round_num + 1
+            elif idx < 6:
+                round_num = max_round_num + 2
+            else:
+                round_num = max_round_num + 3
+        elif len(bracket_pairs) == 3:
+            if idx < 2:
+                round_num = max_round_num + 1
+            else:
+                round_num = max_round_num + 2
+        else:
+            round_num = max_round_num + 1
+
+        row = {
+            'p1_name_raw' : p1_name_raw,
+            'p2_name_raw' : p2_name_raw,
+            'result_raw' : result_raw,
+            'round_num' : round_num,
+            'event_id' : event_id,
+            'elim' : 1,
+            'vs' : 'vs.'
+        }
+        print row
+        results_table.append(row)
+
+    upload_round_results(results_table, event_id, max_round_num + 1)
+
+def all_rounds_info(soup, event_id):
+    return [(clean_magic_link(el['href']), event_id, int(el.text)) for el in soup.find('p', text = 'RESULTS').parent.find_all('a')]
+
+def pre_process_event_link(event_link):
     event_id = event_link.rpartition('/')[2]
     r = requests.get(event_link)
     if r.status_code is 200:
         soup = BeautifulSoup(r.text)
-        return [(clean_magic_link(el['href']), event_id, int(el.text)) for el in soup.find('p', text = 'RESULTS').parent.find_all('a')]
+        return (soup, event_id)
     else:
         r.raise_for_status()
+        return
+
+def process_event_link(event_link):
+    soup, event_id = pre_process_event_link(event_link)
+    print 'Deleting existing rows for event {}'.format(event_id)
+    cursor = Cursor()
+    cursor.execute("delete from {} where event_id='{}'".format(RAW_TABLE_NAME, event_id))
+    cursor.close()
+    
+    failed_links = []
+    try:
+        rounds_info = all_rounds_info(soup, event_id)
+        print 'Round info parsed for event {}'.format(rounds_info[0][1])
+        for round_ in rounds_info:
+            try:
+                process_results_link(*round_)
+                print '>>>>>>{} Round {} Successfully Processed<<<<<<'.format(round_[1], round_[2])
+            except Exception as error:
+                print error
+                print 'XXXXXX{} Round {} Failed XXXXXXX'.format(round_[1], round_[2])
+                failed_links.append(round_[0])
+        elim_results(soup, event_id, max([info[2] for info in rounds_info]))
+        print ''
+        if len(failed_links) > 0:
+            print 'Event {} Incomplete :('.format(rounds_info[0][1])
+        else:
+            print 'Event {} Successfully Processed!'.format(rounds_info[0][1]) 
+    except Exception as error:
+        print error
+        print 'Event {} Failed :('.format(rounds_info[0][1])
+        failed_links.append(event_link)
+    return failed_links
 
 def parse_row(soup, round_num, event_id):
     # we assume rows are either of the format RAW_COL_NAMES or 'table_id','p1_name_raw','results_raw','vs','p2_name_raw'
@@ -49,6 +148,7 @@ def parse_row(soup, round_num, event_id):
         return None
     values.append(round_num)
     values.append(event_id)
+    values.append(0)
     results = dict(zip(RAW_COL_NAMES, values))
     return results
 
@@ -59,56 +159,9 @@ def process_results_link(link, event_id, round_num):
     else:
         r.raise_for_status()
         return
-    print '==========Processing Results for Event {}, Round {}=========='.format(event_id, round_num)
     results_table = [parse_row(row, round_num, event_id) for row in soup.find('table').find_all('tr') if parse_row(row, round_num, event_id) is not None]
-    cursor = Cursor()
-    print 'Deleting existing rows for Event {}, Round {}'.format(event_id, round_num)
-    cursor.execute("delete from {} where event_id='{}' and round_num={}".format(RAW_TABLE_NAME, event_id, round_num))
-    print 'Writing {} rows'.format(len(results_table))
-    cursor.insert(RAW_TABLE_NAME, results_table)
-    cursor.close()
-    cursor = Cursor()
-    print 'New {} row count: {}'.format(RAW_TABLE_NAME, cursor.execute('select count(1) from {}'.format(RAW_TABLE_NAME))[0][0])
-    cursor.close(commit=False)
-    return 0
+    assert len(results_table) > 0, 'no results for event {}, round [}'.format(event_id, round_num)
+    upload_round_results(results_table, event_id, round_num)
 
-def process_results_link_slow(link, driver, event_id, round_num):
-    cursor = Cursor()
-    driver.get(link)
-    try:
-        driver.find_element_by_class_name('fsrDeclineButton').click()
-    except:
-        pass
-    try:
-        warning = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.ID, 'cookies-warning')))
-        warning.find_element_by_tag_name('button').click()
-    except:
-        pass
-    print '==========Processing Results for Event {}, Round {}=========='.format(event_id, round_num)
-    rows = []
-    count = 0
-    while(True):
-        count += 1
-        table = driver.find_element_by_id('DataTables_Table_0')
-        rows.extend([row.get_attribute('outerHTML') for row in table.find_elements_by_tag_name('tr')])
-        next_ = driver.find_element_by_id('DataTables_Table_0_next')
-        if 'disabled' in next_.get_attribute('class'):
-            break
-        try:
-            next_.click()
-        except:
-            return rows, driver
-    results_table = []
-    for row in rows:
-        results_table.append(parse_row(row, round_num, event_id))
-    print 'Deleting existing rows for Event {}, Round {}'.format(event_id, round_num)
-    cursor.execute("delete from {} where event_id='{}' and round_num={}".format(RAW_TABLE_NAME, event_id, round_num))
-    cursor.insert(RAW_TABLE_NAME, results_table)
-    cursor.close()
-    cursor = Cursor()
-    print 'New {} row count: {}'.format(RAW_TABLE_NAME, cursor.execute('select count(1) from {}'.format(RAW_TABLE_NAME))[0][0])
-    cursor.close(commit=False)
-
-    return 1
 
 
--- script.py
+++ script.py
@@ -4,29 +4,9 @@ import process
 links = process.all_event_links()
 
 failed_links = []
-success_links = []
 
-for link in links:
-    print ''
-    try:
-        fail_flag = False
-        rounds_info = process.all_rounds_info(link)
-        print 'Round info parsed for event {}'.format(rounds_info[0][1])
-        for round_ in rounds_info:
-            try:
-                process.process_results_link(*round_)
-                print '>>>>>>{} Round {} Successfully Processed<<<<<<'.format(round_[1], round_[2])
-            except:
-                print 'XXXXXX{} Round {} Failed XXXXXXX'.format(round_[1], round_[2])
-                fail_flag = True
-        print ''
-        if fail_flag:
-            failed_links.append(link)
-        else:
-            success_links.append(link)
-            print 'Event {} Successfully Processed!'.format(rounds_info[0][1]) 
-    except:
-        failed_links.append(link)
+for link in links[:3]:
+  failed_links.extend(process.process_event_link(link))
 
 print ''
 print 'Failed Links:'
