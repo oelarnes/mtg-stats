d09abf91d593a290f472de07f64a03d2252508b9
Joel Barnes <oelarnes@gmail.com>
2015-11-30 23:45:54 -0500

    standardize names added here

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
 def str_reverse(s):
     char_list = [s[-j-1] for j in range(len(s))]
     return ''.join(char_list)
+
+def standardize_name(name):
+    first_last = name.split(' ')
+    return ' '.join(first_last[1:]) + ', ' + first_last[0]
+
